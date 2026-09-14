24f8e6e957e0acbe3907699c1e48567e7b39136f
mnsour <mnsouraldossari@gmail.com>
2026-09-14 13:03:59 +0300

    activity 5

--- a.java
+++ a.java
@@ -3,5 +3,6 @@ public  static void main(String[] args){
     System.out.println("Hello, world");
      System.out.println("Hello, java");
       System.out.println("Hello, activity 5");
+      System.out.println("Hello, activity 5"); 
 
 }}
